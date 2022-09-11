f6718d3f51bcbe03392516551aae09c846a3a72b
Tilman Adler <tilman-git@t-animal.de>
2022-09-11 13:24:15 +0200

    Update cloud combination script

--- scripts/combine-with-clouds.sh
+++ scripts/combine-with-clouds.sh
@@ -2,14 +2,9 @@
 
 cd $(dirname $0)
 
-#curl http://xplanetclouds.com/free/local/clouds_2048.jpg -z clouds_2048.jpg -o clouds_2048.jpg --silent --location
-#curl https://www.ssec.wisc.edu/data/comp/latest_moll.gif -z clouds_640.gif -o clouds_640.gif --silent --location
-curl https://data.ventusky.com/2022/08/30/icon/whole_world/hour_10/icon_oblacnost_20220830_10.jpg -z clouds_720.jpg -o clouds_720.jpg --silent --location
+convert ../cloud-downloads/$(ls -1 ../cloud-downloads/ | tail -n1) -level 14.8,85.6%,0.56 ../earth_clouds_leveled.png
 
+sed -s s/MONTH/$(date +%m)/ config.template > config
+xplanet -config config -body earth -geometry 5400x2700 -output ../earth_terminator_clouds_full.jpg -num_times 1 -projection rectangular  2>&1 | grep -v Resizing | grep -v performance
 
-#only for ventusky:
-convert clouds_720.jpg -resize 5400x2700 -blur 10x30 -auto-level clouds_5400.jpg
-
-sed -s s/MONTH/01/ config.ventusky.template > config
-xplanet -config config -body earth -geometry 5400x2700 -output earth_terminator_full.jpg -num_times 1 -projection rectangular  2>&1 | grep -v Resizing | grep -v performance
-convert -crop 5400x2500+0+100 earth_terminator_full.jpg earth_terminator.jpg
+convert -crop 5400x2500+0+100 ../earth_terminator_clouds_full.jpg ../earth_with_clouds_final.jpg
--- scripts/download-clouds.sh
+++ scripts/download-clouds.sh
@@ -2,25 +2,38 @@
 
 set -e
 
-cd $(dirname $0)/../
+cd "$(dirname "$0")/../"
 
 ZOOM=2
 COLS=3
 ROWS=3
 
-DATE=20220908
-TIME=150000
+if [[ $# -lt 2 ]]; then 
+  echo "usage: $0 <date as YYYYMMDD> hour"
+  echo "To download newest use: $0 \$(date +%Y%m%d) \$(date +%H)"
+  exit 1
+fi
+
+DATE="$1"
+TIME="$2"0000
 
 BASE="https://re.ssec.wisc.edu/api/image?products=globalir_${DATE}_${TIME}&equirectangular=true"
 
 mkdir -p patches
 rm -f patches/*
 
-for COL in $(seq 0 "$(calc $COLS*2+1)" ); do 
-  for ROW in $(seq 0 "$(calc $ROWS)" ); do
-    wget "$BASE&z=$ZOOM&x=$COL&y=$ROW" -O "patches/zoom-$ZOOM--row-$ROW--col-$COL.png"
+DOWNLOADED=0
+TOTAL=$(((COLS*2+2)*(ROWS+1)))
+
+for COL in $(seq 0 "$((COLS*2+1))" ); do 
+  for ROW in $(seq 0 "$ROWS" ); do
+    wget --quiet "$BASE&z=$ZOOM&x=$COL&y=$ROW" -O "patches/zoom-$ZOOM--row-$ROW--col-$COL.png"
+    echo -ne "\rDownloaded $DOWNLOADED of $TOTAL"
+    DOWNLOADED=$((DOWNLOADED+1))
   done
 done
 
+
+echo -e "\rDownloads done. Stitching..."
 cd patches
-montage -mode concatenate -tile 8x4 zoom* ../clouds/clouds_2048_$DATE_$TIME.png
+montage -mode concatenate -tile 8x4 zoom* "../cloud-downloads/clouds_2048_${DATE}_${TIME}.png"
