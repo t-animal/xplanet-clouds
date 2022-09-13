6873b802f58d5195b410fc558f31430d3f2fdc50
Tilman Adler <tilman-git@t-animal.de>
2022-09-13 21:21:07 +0200

    Add download switch and update config

--- scripts/combine-with-clouds.sh
+++ scripts/combine-with-clouds.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-cd $(dirname $0)
+cd $(dirname $0)/..
 
-convert ../cloud-downloads/$(ls -1 ../cloud-downloads/ | tail -n1) -level 14.8,85.6%,0.56 ../earth_clouds_leveled.png
+if [[ "$1" == "--preprocess" ]]; then
+  convert cloud-downloads/$(ls -1 cloud-downloads/ | tail -n1) -scale 5400x2500 -level 14.8,85.6%,0.56 earth_clouds_scaled_leveled.png
+fi
 
-sed -s s/MONTH/$(date +%m)/ config.template > config
-xplanet -config config -body earth -geometry 5400x2700 -output ../earth_terminator_clouds_full.jpg -num_times 1 -projection rectangular  2>&1 | grep -v Resizing | grep -v performance
+if [[ "$1" == "--download" ]]; then
+  wget -N https://t-animal.github.io/xplanet-clouds/earth_clouds_leveled_scaled.png
+fi
 
-convert -crop 5400x2500+0+100 ../earth_terminator_clouds_full.jpg ../earth_with_clouds_final.jpg
+sed -s s/MONTH/$(date +%m)/ scripts/config.template > config
+xplanet -config config -body earth -geometry 5400x2700 -output earth_terminator_clouds_full.jpg -num_times 1 -projection rectangular  2>&1 | grep -v Resizing | grep -v performance
+
+convert -crop 5400x2500+0+100 earth_terminator_clouds_full.jpg earth_with_clouds_final.jpg
